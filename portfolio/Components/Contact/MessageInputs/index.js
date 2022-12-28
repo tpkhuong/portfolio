a5f2578d45d8ed6ffedcc7a31cfb0fa32af6dd85
Toan <tpkhuong@gmail.com>
2022-12-28 03:55:03 -0500

    enjoyed the effort/process of building algorithm fir message inputs form elements mobile.

--- portfolio/Components/Contact/MessageInputs/index.js
+++ portfolio/Components/Contact/MessageInputs/index.js
@@ -3,9 +3,53 @@ import InputsStyles from "./MessageInputs.module.css";
 
 export default function MessageInputs({ children }) {
   return (
-    <div className={MessageInputs[`inputs-sent-message-container`]}>
+    <form role="form" className={InputsStyles[`inputs-sent-message-container`]}>
       {/* inputs */}
+      <div className={InputsStyles[`label-input-container`]}>
+        <label htmlFor="name">_name:</label>
+        <input id="name" type="text" />
+      </div>
+      <div className={InputsStyles[`label-input-container`]}>
+        <label htmlFor="email">_email:</label>
+        <input id="email" type="text" />
+      </div>
+      <div className={InputsStyles[`label-input-container`]}>
+        <label htmlFor="message">_message:</label>
+        <textarea
+          name="user-message"
+          id="message"
+          cols="30"
+          rows="10"
+        ></textarea>
+      </div>
+      <div>
+        <button
+          className={InputsStyles[`submit-btn`]}
+          aria-label="submit message"
+        >
+          submit-message
+        </button>
+        {/* modal container */}
+      </div>
       {/* sent message */}
+    </form>
+  );
+}
+
+function SentMessageModal({ children }) {
+  return (
+    <div className={InputsStyles[`sent-message-modal-container`]}>
+      {/* modal */}
+      <div role="dialog" aria-modal="true" aria-labelledby="sent-message-modal">
+        <h2 className="visually-hidden">Message Sent</h2>
+        <h2 className={InputsStyles[`title`]}>Thank You!</h2>
+        <p className={InputsStyles[`description`]}>
+          Your message has been accepted. You will recieve answer really soon!
+        </p>
+        <button className={InputsStyles[`new-message-btn`]} role="button">
+          send-new-message
+        </button>
+      </div>
     </div>
   );
 }
