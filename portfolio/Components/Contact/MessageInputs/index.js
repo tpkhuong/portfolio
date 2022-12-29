bbb450cbcf664536601e463768cd7d186381f4b2
Toan <tpkhuong@gmail.com>
2022-12-29 03:52:51 -0500

    enjoyed the effort/process of building algorithm for mobile sent message modal and show/hide modal when user click submit btn.

--- portfolio/Components/Contact/MessageInputs/index.js
+++ portfolio/Components/Contact/MessageInputs/index.js
@@ -2,41 +2,63 @@ import React from "react";
 import InputsStyles from "./MessageInputs.module.css";
 
 export default function MessageInputs({ children }) {
+  const [showModal, setModalState] = React.useState(false);
   return (
-    <form role="form" className={InputsStyles[`inputs-sent-message-container`]}>
-      {/* inputs */}
-      <div className={InputsStyles[`label-input-container`]}>
-        <label htmlFor="name">_name:</label>
-        <input id="name" type="text" />
-      </div>
-      <div className={InputsStyles[`label-input-container`]}>
-        <label htmlFor="email">_email:</label>
-        <input id="email" type="text" />
-      </div>
-      <div className={InputsStyles[`label-input-container`]}>
-        <label htmlFor="message">_message:</label>
-        <textarea
-          name="user-message"
-          id="message"
-          cols="30"
-          rows="10"
-        ></textarea>
-      </div>
-      <div>
+    <div className={InputsStyles[`inputs-modal-container`]}>
+      <form
+        role="form"
+        data-showmodal="false"
+        className={InputsStyles[`inputs-sent-message-container`]}
+      >
+        {/* inputs */}
+        <div className={InputsStyles[`label-input-container`]}>
+          <label htmlFor="name">_name:</label>
+          <input id="name" type="text" />
+        </div>
+        <div className={InputsStyles[`label-input-container`]}>
+          <label htmlFor="email">_email:</label>
+          <input id="email" type="text" />
+        </div>
+        <div
+          className={`${InputsStyles[`label-input-container`]} ${
+            InputsStyles[`margin-block-start-42px`]
+          }`}
+        >
+          <label htmlFor="message">_message:</label>
+          <textarea name="" id="message" cols="30" rows="10"></textarea>
+        </div>
+        {/* sent message */}
         <button
+          type="button"
+          onClick={(event) => {
+            if (!showModal) {
+              // show modal
+              setModalState(true);
+              return;
+            }
+            if (showModal) {
+              // hide modal
+              setModalState(false);
+              return;
+            }
+          }}
           className={InputsStyles[`submit-btn`]}
           aria-label="submit message"
         >
           submit-message
         </button>
+        {/* <div className={InputsStyles[`button-modal-container`]}>
+        </div> */}
         {/* modal container */}
-      </div>
-      {/* sent message */}
-    </form>
+      </form>
+      {showModal ? (
+        <SentMessageModal hideModalFunc={{ showModal, setModalState }} />
+      ) : null}
+    </div>
   );
 }
 
-function SentMessageModal({ children }) {
+function SentMessageModal({ children, hideModalFunc }) {
   return (
     <div className={InputsStyles[`sent-message-modal-container`]}>
       {/* modal */}
@@ -46,7 +68,14 @@ function SentMessageModal({ children }) {
         <p className={InputsStyles[`description`]}>
           Your message has been accepted. You will recieve answer really soon!
         </p>
-        <button className={InputsStyles[`new-message-btn`]} role="button">
+        <button
+          onClick={(event) => {
+            console.log(hideModalFunc.showModal);
+            console.log(hideModalFunc.setModalState);
+          }}
+          className={InputsStyles[`new-message-btn`]}
+          role="button"
+        >
           send-new-message
         </button>
       </div>
